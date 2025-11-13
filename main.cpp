7e9c1040de7292bc80cb28866694a95cf10516f0
Hasham Tanveer <hashamtanvr41@gmail.com>
2025-11-13 18:53:28 +0500

    Functions prototyping

--- main.cpp
+++ main.cpp
@@ -4,6 +4,13 @@
 
 using namespace std;
 
+void intro();
+void stageOne(int &, int &, int []);
+void stageTwo(int &, int &, int []);
+void stageThree(int, int, int []);
+bool playAgain();
+void showStats(int, int);
+
 int main() {
     srand(time(0));
     int choice, score = 0, energy = 100;
