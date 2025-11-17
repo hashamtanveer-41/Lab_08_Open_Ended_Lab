273c42563e46797645918b396f00f73fe09b1d92
Hasham Tanveer <hashamtanvr41@gmail.com>
2025-11-17 20:55:26 +0500

    Comments were added to functions displayIntro, showMenu and moveLocation

--- main.cpp
+++ main.cpp
@@ -71,12 +71,16 @@ int main() {
         return 0;
 
 }
+// Displaying game information to user. Information includes scenario, objective, game instructions, character and
+// location information. After each section user is prompted to enter any key to proceed.
 void displayIntro(){
     string begin;
-
+    // Displaying header of detective game
     cout<<"------------------------------------------------"<<endl;
     cout<<"                 Detective Game"<<endl;
     cout<<"------------------------------------------------"<<endl<<endl;
+
+    // Displaying scenario of case
     cout<<"-------------------Scenario---------------------"<<endl;
     cout << "Welcome detective to case of emerald heirloom.You are detective Sherlock holmes methodical investigator \n"
             "famous for solving murder mysteries. \n"
@@ -85,13 +89,18 @@ void displayIntro(){
             "You are called to uncover the hidden truth before dawn.\n"
             "Time is short, energy is limited and every wrong decision will cost you \n"
             "your reputation.\n"<<endl;
-    cout<<"(Press any key to continue)";
+    // Prompting the user any key and then press enter to proceed to next section
+    cout<<"(Press any key and enter to continue)";
     cin>>begin;
+
+    // Displaying Objective of case
     cout<<"--------------------Objective---------------------"<<endl;
     cout<<"Your goal is to investigate the manor, question suspects and uncover \n"
           "clues to find the real killer before your energy runs out.\n"<<endl;
-    cout<<"(Press any key to continue)";
+    cout<<"(Press any key and enter to continue)";
     cin>>begin;
+
+    // Displaying game instructions which the user must follow
     cout<<"-------------------Game Instructions------------------"<<endl;
     cout<<"Player is suggested to follow the following rules: \n"
           "1. Type the number for your chosen action (1-5)\n"
@@ -100,9 +109,10 @@ void displayIntro(){
           "4. Collect clues to strengthen your final accusation.\n"
           "5. Wrong choices or running out of Energy means you lose.\n"
           "6. To make final accusations, you have to complete all the locations.\n"<<endl;
-
-    cout<<"(Press any key to continue)";
+    cout<<"(Press any key and enter to continue)";
     cin>>begin;
+
+    // Displaying information of Characters and Locations in house
     cout<<"-------------------Character and Location Introduction------------------"<<endl;
     cout<<"Following are short introductions about characters: \n"
           "\t1.Lady Evelyn Sterling (the widow): Calm but distant\n"
@@ -112,11 +122,14 @@ void displayIntro(){
           "\t2.The Study Room (crime scene)\n"
           "\t3.The dining Room(suspect area)\n"
           "\t4.The Garden Path(hidden clue area)\n"<<endl;
-    cout<<"(Press any key to continue)";
+    cout<<"(Press any key and enter to continue)";
     cin>>begin;
 }
+
+// This function displays the menu to the user and prompts him/her to make the choice and returns the choice
 int showMenu(int stage) {
     int choice;
+    // Displaying menu to the user
     cout << "\nChoose your next action:\n"
     "1. Move to another location\n"
     "2. Check your current status\n"
@@ -125,46 +138,63 @@ int showMenu(int stage) {
     if (stage==3) cout<<"5. Make a final accusation\n";
     else cout<<"5.Continue investigation\n";
      cout<<"6. Exit Game\n";
+    // Prompting user to enter his/her choice
     cout<<"Enter your choice(1 to 6): "<<endl;
     cin>>choice;
+    // Validating the input of the user
     while (choice<1||choice>6) {
         cout<<"Invalid action.Enter your choice: "<<endl;
         cin>>choice;
     }
     return choice;
 }
+
+// This function enables the user to move to other locations of house. Function displays the different locations
+// and prompts the user to choose the locations
 void moveLocation(int& energy, int& stage) {
     int choice;
+    // Displaying the locations
     cout<<"Select the location: \n"
           "1. Study Room \n"
           "2. Dining Room \n"
           "3. Garden Path \n";
+    // Prompting the user to enter the desired location
     cout<<"Enter your choice: "<<endl;
     cin>> choice;
+    // Validating the input of the user
     while (choice<1||choice>3) {
         cout<<"Invalid choice. Enter your choice: "<<endl;
         cin>> choice;
     }
+    // Depleting energy as moving to new location will cost some amount of energy
      energy -=5;
+    // Checking if the energy is less than a certain level then the user is unable to move to new location
     if (energy<=10)cout<<"You are feeling exhausted. Solve as soon as possible. ";
+    // Executing specific case according to the choice of the user
     switch (choice) {
         case 1:
+            // Entering the study room.
             cout<<"You have entered Study Room. The air is humid and tense."<<endl;
             if (stage==1) {
                 cout<<"You will start your first interrogation."<<endl;
+                // Assigning the value of stage to 1
                 stage =1;
             }
             break;
         case 2:
+            // Entering the user to Dining room
             cout<<"You are in Dining room. The staff manager stands by nervously."<<endl;
             if (stage<2) {
+                // Assigning the value of stage to 2
                 stage=2;
                 cout<<"Suspect interrogation stage has unlocked.";
             }
             break;
         case 3:
+            // Entering the Garden
             cout<<"You follow the path to the garden. Its cold air blowing...."<<endl;
             if (stage<3) {
+                // Assigning the value of stage to 3
                 stage = 3;
                 cout<<"Final stage: The truth is waiting! "<<endl;
             }
