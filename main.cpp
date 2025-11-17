b38f36b4185951e277b52feec6d0fc49f34f72cc
asfandkhan8585-cpu <asfandkhan8585@gmail.com>
2025-11-17 23:01:23 +0500

    Stage 3 function was committed

--- main.cpp
+++ main.cpp
@@ -21,12 +21,11 @@ bool gameRunning = true;
 int main() {
     srand(time(0));
     int choice;
-    stageTwo(energy,reputation,clues);
     return 0;
 
 
-
 }
+
 void stageTwo(int &energy, int &reputation, int []) {
 int choice;
 
@@ -50,10 +49,10 @@ int choice;
             int decision;
             cout << "choose an option"<<endl;
             cin >> decision;
-            if (decision==1) {
-                if (clues[1]==0) {
+            if (decision==1){
+                if (clues[0]==0) {
                     cout << "She says I was in the resting room. With her voice trembling"<<endl;
-                    clues[1]=1;
+                    clues[0]=1;
                     reputation+=20;
                     energy-=10;
                     cout << "clue gained: her voice is trembling"<<endl;
@@ -82,10 +81,10 @@ int choice;
             cout << "2. Ask about the missing heirloom"<<endl;
                 cout << "choose an option"<<endl;
                 cin >> decision_1;
-                if (decision_1==1) {
-                    if (clues[2]==0) {
+                if (decision_1==1){
+                    if (clues[1]==0){
                         cout << "I heard footsteps near the study table. But I am not sure"<<endl;
-                        clues[2]=1;
+                        clues[1]=1;
                         cout << "clue gained: footsteps near the study table"<<endl;
                         energy-=10;
                         reputation+=20;
@@ -123,4 +122,60 @@ int choice;
     }
     cout << "stage 2 completed. Now on to the garden path"<<endl;
 
+}
+void stageThree(int &energy, int &reputation, int[]) {
+    int choice;
+    cout << "You have step into the Garden Path."<<endl<<endl;
+    cout << "Cold wind brushes past you."<<endl;
+    cout << "This is the last chance to prove yourself"<<endl;
+
+
+    cout << ".............What would you like to do?..............."<<endl<<endl;
+    cout << "1. Inspect the muddy footprints"<<endl;
+    cout << "2. Examine the broken fence"<<endl;
+    cout << "3. Search behind the bushes (waste time)"<<endl;
+    cout << "4. Leave the Garden Path (finish investigation)"<<endl;
+    cout << "enter the choice"<<endl;
+    cin >> choice;
+
+    switch (choice) {
+        case 1:
+            if (clues[2] == 0) {
+                cout << "you followed the muddy footprints."<<endl;
+                cout << "they lead away from the Study toward the back fence."<<endl;
+                clues[2] = 1;
+                reputation += 20;
+                energy-=10;
+                cout << "energy lost = 10 joules"<<endl;
+                cout << "reputation gained = 20"<<endl;
+            }
+            break;
+
+        case 2:
+            if (clues[3] == 0){
+                cout << "You inspected the wooden fence."<<endl;
+                cout << "The fence is cracked someone forced their way through."<<endl;
+                clues[3] = 1;
+                reputation += 20;
+                energy-=10;
+                cout << "energy lost = 10 joules"<<endl;
+                cout << "reputation gained = 20"<<endl;
+            }
+            break;
+
+        case 3:
+            cout << "You search the bushes but find nothing useful."<<endl;
+            energy-=10;
+            cout << "energy lost = 10 joules"<<endl;
+            break;
+
+        case 4:
+            cout << "You take one last look at the Garden Path. you have done all you could have and collected all the evidences ."<<endl;
+            break;
+
+        default:
+            cout << "Invalid choice."<<endl;
+            break;
+    }
+    cout << "stage 3 is finished and the game is over";
 }
